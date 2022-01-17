185a2e65e2a6dbfe8a263d31120ac8acfde3e314
nic0mp <nico.mp215@gmail.com>
2022-01-17 11:01:05 -0600

    functions

---
+++ functionNotes.js
@@ -0,0 +1,10 @@
+// FUNCTION DECLARATIONS
+function getReminder() {
+    console.log('Water the plants.')
+  }
+  
+  function greetInSpanish() {
+    console.log('Buenas Tardes.')
+  }
+
+// FUNCTION CALLS
