56f600b4299b3cd025779c0e9739aa953d3b6c50
nic0mp <nico.mp215@gmail.com>
2022-01-17 11:25:25 -0600

    Arguments and parameters

--- functionNotes.js
+++ functionNotes.js
@@ -14,3 +14,12 @@ function sayThanks(){
   sayThanks()
   sayThanks()
   sayThanks()
+
+// ARGUMENTS AND PARAMETERS
+// Add a parameter called name to the function declaration for sayThanks().
+// With name as a parameter, it can be used as a variable in the function body of sayThanks().
+function sayThanks(name) {
+    console.log('Thank you for your purchase ' + name + '! We appreciate your business.');
+  }
+  sayThanks('Cole')
+// ^^Call sayThanks() and pass 'Cole' as an argument to send Cole a personalized thank you message.
