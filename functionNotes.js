c63efc7bad1c2b1e26900b386a5a5273bd80bb3b
nic0mp <nico.mp215@gmail.com>
2022-01-17 22:59:33 -0600

    helper functions

--- functionNotes.js
+++ functionNotes.js
@@ -60,3 +60,24 @@ function monitorCount(rows,columns) {
   return rows * columns
 }
 console.log(monitorCount (5, 4))
+
+// HELPER FUNCTIONS
+function multiplyByNineFifths(number) {
+  return number * (9/5);
+};
+ 
+function getFahrenheit(celsius) {
+  return multiplyByNineFifths(celsius) + 32;
+};
+ 
+getFahrenheit(15);
+
+// ________________
+function monitorCount(rows, columns) {
+  return rows * columns;
+}
+function costOfMonitors(rows, columns) {
+  return monitorCount(rows, columns)*200
+}
+const totalCost = costOfMonitors(5,4)
+console.log(totalCost)
