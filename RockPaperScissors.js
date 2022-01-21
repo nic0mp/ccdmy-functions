85b05fc05e8e077d88333f2252f9ec255ecd8f22
nic0mp <nico.mp215@gmail.com>
2022-01-20 21:24:47 -0600

    rock paper scissors

---
+++ RockPaperScissors.js
@@ -0,0 +1,35 @@
+const getUserChoice = userInput => {
+    userInput = userInput.toLowerCase();
+    if(userInput === 'rock' || userInput === 'paper' || userInput === 'scissor' ){
+      return userInput;
+    } else {
+      console.log('Error')
+    }
+  };
+  console.log(getUserChoice('Paper'))
+  function getComputerChoice() {
+    randomNum = Math.floor(Math.random() * 3);
+    switch(randomNum){
+      case 0:
+        return 'rock';
+        break;
+      case 1:
+        return 'scissors';
+        break;
+      case 2:
+        return 'paper';
+        break;
+    };
+  }
+  console.log(getComputerChoice())
+  function determineWinner(userChoice,computerChoice) {
+    if(userChoice === computerChoice){
+      return 'TIE!';
+    } if(userChoice === 'rock'){
+      if(computerChoice === 'paper'){
+        return 'Computer Won!'
+      } else {
+        return 'User Won!';
+      }
+    }
+  }
