62c2b4f6b57b137fef2c4ae6f47dc807542661ab
nic0mp <nico.mp215@gmail.com>
2022-01-21 21:28:08 -0600

    rock paper scissors

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,12 +1,12 @@
 const getUserChoice = userInput => {
     userInput = userInput.toLowerCase();
-    if(userInput === 'rock' || userInput === 'paper' || userInput === 'scissor' ){
+    if(userInput === 'rock' || userInput === 'paper' || userInput === 'scissors' ){
       return userInput;
     } else {
       console.log('Error')
     }
   };
-  console.log(getUserChoice('Paper'))
+  console.log(getUserChoice('rock'))
   function getComputerChoice() {
     randomNum = Math.floor(Math.random() * 3);
     switch(randomNum){
@@ -20,16 +20,37 @@ const getUserChoice = userInput => {
         return 'paper';
         break;
     };
-  }
-  console.log(getComputerChoice())
-  function determineWinner(userChoice,computerChoice) {
+}
+console.log(getComputerChoice())
+function determineWinner(userChoice,computerChoice) {
     if(userChoice === computerChoice){
-      return 'TIE!';
+    return 'TIE!';
     } if(userChoice === 'rock'){
-      if(computerChoice === 'paper'){
-        return 'Computer Won!'
-      } else {
+        if(computerChoice === 'paper'){
+        return 'Computer Won!';
+        } else {
         return 'User Won!';
-      }
-    } if 
-  }
+        }
+    } if(userChoice === 'paper') {
+        if(computerChoice === 'scissors' ) {
+        return 'Computer Won!';
+            } else {
+        return 'User Won!';
+        }
+    } if(userChoice === 'scissors') {
+        if(computerChoice === 'rock' ) {
+        return 'Computer Won!';
+        } else {
+        return 'User Won!';
+        }
+    }
+};
+console.log(determineWinner('rock','scissors'))
+function playGame() {
+    const userChoice = getUserChoice('scissors');
+    const computerChoice = getComputerChoice();
+    console.log('You threw: ' + userChoice);
+    console.log('Computer threw: ' + computerChoice);
+    console.log(determineWinner(userChoice,computerChoice));
+}
+playGame()
