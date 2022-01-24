b82912797480fa453536349860c4804ec3cd6f82
nic0mp <nico.mp215@gmail.com>
2022-01-23 21:16:53 -0600

    sleep debt calculator

---
+++ SleepDebtCalc.js
@@ -0,0 +1,19 @@
+const getSleepHours = day => {
+    if(day === 'monday') {
+      return 8; 
+    } else if (day === 'tuesday') {
+      return 7;
+    } else if (day === 'wednesday') {
+      return 6;
+    } else if (day === 'thursday') {
+      return 5;
+    } else if (day === 'friday') {
+      return 4;
+    } else if (day === 'saturday') {
+      return 10;
+    } else if (day === 'sunday') {
+      return 9;
+    } else {
+      return 'Please enter a valid day'
+    }
+  };
